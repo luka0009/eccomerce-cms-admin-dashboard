ee128b96603357bfaaa57c93e45d095f7208fac7
luka <lukabarbakadze18@gmail.com>
2023-07-04 23:26:47 +0400

    billboards page

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -15,7 +15,7 @@ const BillboardPage = async ({
 	return (
 		<div className="flex-col">
 			<div className="flex-1 space-y-4 p-8 pt-6">
-				<BillboardForm />
+				<BillboardForm initialData={billboard}/>
 			</div>
 		</div>
 	);
